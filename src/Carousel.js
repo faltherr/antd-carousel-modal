b3a97dde16e22783c0ea23084e15a6d56feaa955
Forest Altherr <forest.altherr@biofiredx.com>
2021-04-27 08:16:23 -0600

    Toggle slides for modal

--- src/Carousel.js
+++ src/Carousel.js
@@ -1,31 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button, Modal } from "antd";
 import Slide from "./Slide";
 import "./carousel.css";
 
 const mySlides = [
-  { releaseImage: "", title: "", description: "1" },
-  { releaseImage: "", title: "", description: "2" },
-  { releaseImage: "", title: "", description: "3" },
+  {
+    releaseImage:
+      "https://images.unsplash.com/photo-1612392062631-94dd858cba88?ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80",
+    title: "Release 1",
+    description: "1",
+  },
+  {
+    releaseImage:
+      "https://images.unsplash.com/photo-1619359037135-4d8b70426833?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80",
+    title: "Release 2",
+    description: "2",
+  },
+  {
+    releaseImage:
+      "https://images.unsplash.com/photo-1619442407451-bf6cf721301f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=80",
+    title: "Release 3",
+    description: "3",
+  },
 ];
 
 export default function Carousel(props) {
+  const [currentSlide, setCurrentSlide] = useState(0);
   const { isModalVisible, handleCloseModal } = props;
   return (
     <Modal
+      className="carousel-modal"
       visible={isModalVisible}
       onOk={handleCloseModal}
       onCancel={handleCloseModal}
       closable={false}
-      footer={mySlides.map((s) => (
-        <Button>{s.description}</Button>
+      footer={mySlides.map((s, i) => (
+        <button
+          onClick={() => setCurrentSlide(i)}
+          key={i}
+          className={`slide-toggle-button ${
+            currentSlide === i ? "active" : ''
+          }`}
+        >
+          <span></span>
+        </button>
       ))}
     >
-      {mySlides.map((s) => (
-        <Slide slideDetails={s} />
+      {mySlides.map((s, i) => (
+        <Slide releaseDetails={s} slideActive={currentSlide === i} />
       ))}
-      <div>
-        <Button>Got It</Button>
+      <div className="modal-confirm-container">
+        <Button onClick={() => handleCloseModal()}>Got It</Button>
       </div>
     </Modal>
   );
--- src/Slide.js
+++ src/Slide.js
@@ -1,43 +1,45 @@
-import React from 'react';
+import React from "react";
 
-const CarouselSlide = props => {
-  const {
-    slideDetails,
-    classes,
-    featureTitle,
-    releaseText,
-    releaseImage,
-    handleCloseModal,
-  } = props;
-  console.log('Slide rendered')
+const CarouselSlide = (props) => {
+  const { slideActive, releaseDetails } = props;
+  console.log("Slide rendered", slideActive);
+  if (!slideActive) {
+    return null;
+  }
   return (
     <div
+      className={"slide-container"}
       // className={
       //   releaseImage ? classes.slideContainer : classes.noImageSlideContainer
       // }
     >
-      {releaseImage ? (
-        <div>
+      {releaseDetails ? (
+        <div
+          className={`image-container ${slideActive ? "active-slide" : null}`}
+        >
           <img
-            src={releaseImage}
+            src={releaseDetails.releaseImage}
             alt="screenshot of release note"
-            // className={classes.releaseImageItem}
+            className={"release-image-item"}
           />
         </div>
       ) : null}
-      <div 
+      <div
       // className={classes.slideTextContentContainer}
       >
+      <div className="feature-all-text-container">
         <div
-          // className={
-          //   releaseImage
-          //     ? classes.featureTitleTextContainer
-          //     : `${classes.featureTitleTextContainer} ${classes.noImage}`
-          // }
+        // className={
+        //   releaseImage
+        //     ? classes.featureTitleTextContainer
+        //     : `${classes.featureTitleTextContainer} ${classes.noImage}`
+        // }
+        className="feature-title-text-container"
         >
-          <h4>Slide Title</h4>
+          <h4>{releaseDetails.title}</h4>
         </div>
-        <p>Slide description Text</p>
+        <p>{releaseDetails.description}</p>
+      </div>
       </div>
       {/* <Button
         variant="contained"
@@ -50,83 +52,83 @@ const CarouselSlide = props => {
     </div>
   );
 };
-const styles = theme => ({
+const styles = (theme) => ({
   text: {
     MsGridRow: 2,
     color: theme.palette.tones.darkBlueGrey,
   },
   imageContainer: {
-    paddingTop: '15px',
-    paddingBottom: '15px',
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
+    paddingTop: "15px",
+    paddingBottom: "15px",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
     backgroundColor: theme.palette.tones.lightBlueGrey,
     MsGridRow: 1,
     MsGridColumn: 1,
-    gridArea: 'imageContainer',
+    gridArea: "imageContainer",
   },
   releaseImageItem: {
-    maxHeight: '100%',
-    maxWidth: '100%',
+    maxHeight: "100%",
+    maxWidth: "100%",
   },
   slideContainer: {
-    width: '100%',
-    height: '100%',
-    display: 'grid',
+    width: "100%",
+    height: "100%",
+    display: "grid",
     gridTemplateAreas: `
         'imageContainer'
         'slideTextContentContainer'
         'closeModalButton'
       `,
-    '& >*': {
-      marginBottom: '12px',
+    "& >*": {
+      marginBottom: "12px",
     },
   },
   noImageSlideContainer: {
-    width: '100%',
-    height: '100%',
-    display: 'grid',
-    gridAutoRows: '19fr 1fr',
+    width: "100%",
+    height: "100%",
+    display: "grid",
+    gridAutoRows: "19fr 1fr",
     gridTemplateAreas: `
         'slideTextContentContainer'
         'closeModalButton'
       `,
-    '& >*': {
-      marginBottom: '12px',
+    "& >*": {
+      marginBottom: "12px",
     },
   },
   featureTitleTextContainer: {
-    display: 'flex',
+    display: "flex",
     MsGridRow: 1,
-    justifyContent: 'center',
-    alignItems: 'flex-end',
-    width: '80%',
+    justifyContent: "center",
+    alignItems: "flex-end",
+    width: "80%",
   },
   noImage: {
-    borderBottom: '1px solid',
+    borderBottom: "1px solid",
     borderImageSlice: 1,
-    borderImageSource: 'linear-gradient(to left, #acb4b7, #37474f, #acb4b7)',
+    borderImageSource: "linear-gradient(to left, #acb4b7, #37474f, #acb4b7)",
   },
   slideTextContentContainer: {
-    display: 'grid',
-    gridAutoRows: '1fr minmax(50%, auto)',
-    justifyItems: 'center',
+    display: "grid",
+    gridAutoRows: "1fr minmax(50%, auto)",
+    justifyItems: "center",
     MsGridRow: 2,
     MsGridColumn: 1,
-    gridArea: 'slideTextContentContainer',
-    paddingRight: '45px',
-    paddingLeft: '45px',
+    gridArea: "slideTextContentContainer",
+    paddingRight: "45px",
+    paddingLeft: "45px",
   },
   closeModalButton: {
-    display: 'flex, -webkit-box, -ms-flexbox',
-    MsGridColumnAlign: 'center',
-    justifySelf: 'center',
-    alignSelf: 'center',
-    height: '35px',
+    display: "flex, -webkit-box, -ms-flexbox",
+    MsGridColumnAlign: "center",
+    justifySelf: "center",
+    alignSelf: "center",
+    height: "35px",
     MsGridRow: 3,
     MsGridColumn: 1,
-    gridArea: 'closeModalButton',
+    gridArea: "closeModalButton",
   },
 });
 
